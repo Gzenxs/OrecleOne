b081246209e0e575877af7528cb015a284334eef
Gzenxs <gzenxs@gmail.com>
2024-02-05 16:34:04 -0600

    condicional if

--- 2034-logica-programacion-1/app.js
+++ 2034-logica-programacion-1/app.js
@@ -1,2 +1,7 @@
-alert('Hola Mundo');
+let numeroSecreto = 6;
 let numerosUsuario = prompt("Me indicas un nemero por favor");
+
+
+if (numeroSecreto == numerosUsuario) {
+	alert('Acertaste el numero Secreto');
+}
