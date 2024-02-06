abceee8ce1a3e156556b6786ffc340c7bb1fc6f2
Gzenxs <gzenxs@gmail.com>
2024-02-06 14:28:49 -0600

    Desafios Muestra una alerta con el mensaje ¡Bienvenida y bievenido a nuestro sitio web!

--- 2034-logica-programacion-1/app.js
+++ 2034-logica-programacion-1/app.js
@@ -1,7 +1,9 @@
-let numeroSecreto = 6;
-let numerosUsuario = prompt("Me indicas un nemero por favor");
+//
+//let numeroSecreto = 6;
+//let numerosUsuario = prompt("Me indicas un nemero por favor");
 
 
-if (numeroSecreto == numerosUsuario) {
-	alert('Acertaste el numero Secreto');
-}
+//if (numeroSecreto == numerosUsuario) {
+//	alert('Acertaste el numero Secreto');
+//}
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
