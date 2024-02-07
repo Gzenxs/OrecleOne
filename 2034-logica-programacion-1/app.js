1423b2eb200d77ea038a5a20ff7a393d0ed9422f
Gzenxs <gzenxs@gmail.com>
2024-02-07 13:26:25 -0600

    Add desafio.js

--- 2034-logica-programacion-1/app.js
+++ 2034-logica-programacion-1/app.js
@@ -1,9 +1,33 @@
-//
-//let numeroSecreto = 6;
-//let numerosUsuario = prompt("Me indicas un nemero por favor");
+
+let numeroSecreto = 6;
+let numerosUsuario = prompt("Me indicas un nemero por favor");
 
 
-//if (numeroSecreto == numerosUsuario) {
-//	alert('Acertaste el numero Secreto');
-//}
-alert("¡Bienvenida y bievenido a nuestro sitio web!");
+if (numeroSecreto == numerosUsuario) {
+	alert('Acertaste el numero Secreto');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2034-logica-programacion-1/desafio1.js
@@ -0,0 +1,31 @@
+
+//let edad = prompt("dime cual es tu edad");
+
+//alert("¡Bienvenida y bievenido a nuestro sitio web!");
+//alert("¡Error! Completa todos los campos");
+// 01 Muestra una alerta con el mensaje "¡Bienvenida y bievenido a nuestro sitio web!".
+// alert('¡Bienvenido a nuestro sitio web!');
+// 02 Declara una variable llamada nombre y asígnale el valor "Luna".
+// let nombre = "Luna";
+// 03 Crea una variable llamada `edad`y asígnale el valor 25.
+// let edad = 25;
+// 04 Establece una variable numeroDeVentas y asígnale el valor 50.
+// let numeroDeVentas = 50;
+// 05 Establece una variablu saldoDisponible y asígnale el valor 1000.
+// let saldoDisponible = 1000;
+// 06 Muestra una alerta con el texto "¡Error! Completa todos los campos".
+// alert('¡Error! Preencha todos los campos');
+// 07 Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+// let mensajeDeError = "¡Error! Completa todos los campos";
+// alert(mensajeDeError);
+// 08 Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .
+// let nombre = prompt("Dime cual es tu nombre");
+// 09 Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable `edad`.
+// let edad = prompt("Ingrese su Edad");
+// 10 Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+//
+// let edad = prompt("Ingresa tu edad");
+//
+//if (edad >= 18) {
+//	alert("¡Puedes obtener tu licencia de conducir!");
+//}
